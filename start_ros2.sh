1b31bd2f3e7bebedc7883380af8728252a09f144
Riccardo Passoni <r.passoni@surrey.ac.uk>
2026-09-13 14:20:52 +0100

    add ROS_DOMAIN_ID and ip

--- start_ros2.sh
+++ start_ros2.sh
@@ -3,12 +3,18 @@
 # chmod +x start_turtlebot.sh
 # ./start_turtlebot.sh
 set -e
+source /opt/ros/jazzy/setup.bash
 
 export TURTLEBOT3_MODEL=burger
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export ROS_DOMAIN_ID=77
+
+ROBOT_IP=$(ip -4 addr show wlan0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1 || true)
 
 echo "TURTLEBOT3_MODEL=$TURTLEBOT3_MODEL"
 echo "RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION"
+echo "ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
+echo "Robot IP (wlan0): ${ROBOT_IP:-NOT FOUND}"
 echo "Launching robot"
 echo ""
 
